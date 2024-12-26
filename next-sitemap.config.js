1d299f394307cc5bf029a2b2269961505af80201
Emmanuel <65783304+EmmanuelOta@users.noreply.github.com>
2024-12-26 16:28:26 +0100

    Generated sitemap for google search console

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	siteUrl: "https://wordifyimage.vercel.app",
+	generateRobotsTxt: true, // (optional) Generate a robots.txt file
+	sitemapSize: 5000, // (optional) Limit the number of URLs per sitemap file
+};
--- next-sitemap.config.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from "next-sitemap";
-
-const config: IConfig = {
-	siteUrl: process.env.SITE_URL || "https://yourdomain.com",
-	generateRobotsTxt: true, // Generate a robots.txt file
-	sitemapSize: 5000, // Limit the number of URLs per sitemap file
-};
-
-export default config;
